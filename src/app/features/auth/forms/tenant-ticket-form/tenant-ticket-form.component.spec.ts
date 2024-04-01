c13992a85b5f380848883f91300c8a620071385c
H20Beetle <waterbeetle091@gmail.com>
2024-04-01 18:16:10 -0500

    Update tenant-ticket-form.component.spec.ts

--- src/app/features/auth/forms/tenant-ticket-form/tenant-ticket-form.component.spec.ts
+++ src/app/features/auth/forms/tenant-ticket-form/tenant-ticket-form.component.spec.ts
@@ -1,23 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ReactiveFormsModule } from "@angular/forms";
+import { TenantTicketFormComponent } from "./tenant-ticket-form.component";
 
-import { TenantTicketFormComponent } from './tenant-ticket-form.component';
+describe("TenantTicketFormComponent", () => {
+	let component: TenantTicketFormComponent;
+	let fixture: ComponentFixture<TenantTicketFormComponent>;
 
-describe('TenantTicketFormComponent', () => {
-  let component: TenantTicketFormComponent;
-  let fixture: ComponentFixture<TenantTicketFormComponent>;
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [TenantTicketFormComponent]
+		}).compileComponents();
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [TenantTicketFormComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(TenantTicketFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(TenantTicketFormComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it("should create", () => {
+		expect(component).toBeTruthy();
+	});
 });
