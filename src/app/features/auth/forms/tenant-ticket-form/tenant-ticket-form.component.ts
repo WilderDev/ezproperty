0d0d93daffb6f2403f4ea36e047dfcdba0bf7ef8
H20Beetle <waterbeetle091@gmail.com>
2024-04-04 17:56:16 -0500

    Update tenant-ticket-form.component.ts

--- src/app/features/auth/forms/tenant-ticket-form/tenant-ticket-form.component.ts
+++ src/app/features/auth/forms/tenant-ticket-form/tenant-ticket-form.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 @Component({
-  selector: 'app-tenant-ticket-form',
-  templateUrl: './tenant-ticket-form.component.html',
-  styleUrl: './tenant-ticket-form.component.scss'
+	selector: "app-tenant-ticket-form",
+	templateUrl: "./tenant-ticket-form.component.html",
+	styleUrl: "./tenant-ticket-form.component.scss"
 })
-export class TenantTicketFormComponent {
+export class TenantTicketFormComponent implements OnInit {
+	tenantIssueForm: FormGroup;
 
+	ngOnInit(): void {
+		this.tenantIssueForm = new FormGroup({
+			username: new FormControl(null, Validators.required),
+			email: new FormControl(null, [Validators.required, Validators.email]),
+			gender: new FormControl("")
+		});
+	}
 }
