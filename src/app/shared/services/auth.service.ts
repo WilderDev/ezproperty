b4cfda3ad4731bc60bd2dba4e4fd05d0f840c8f4
Blake Graviett <blake.graviett@gmail.com>
2024-04-07 17:06:32 -0500

    fix auth

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,87 +1,65 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, tap } from 'rxjs';
-import { Router } from '@angular/router';
-import { CookieService } from 'ngx-cookie-service';
-import { environment } from '../../../environments/environment';
-
-
-// Model for User
-export interface User {
-  id: number;
-  username: string;
-}
-
-// Model for User being Registered
-export interface CreateUser {
-  username: string;
-  email: string;
-  password: string;
-}
-
-// Model for User being Logged In
-export interface LoginUser {
-  email: string;
-  password: string;
-}
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { BehaviorSubject, tap } from "rxjs";
+import { Router } from "@angular/router";
+import { CookieService } from "ngx-cookie-service";
+import { environment } from "../../../environments/environment";
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: "root"
 })
 export class AuthService {
-  private user$ = new BehaviorSubject<User | undefined | null>(null);
+	constructor(
+		private httpClient: HttpClient,
+		private cookieService: CookieService,
+		private router: Router
+	) {}
 
+	// Function to Register New User
+	register({ username, email, password }) {
+		console.log(username, email, password);
 
+		return this.httpClient.post(
+			`${environment.API_URL}/auth/register`,
+			{
+				username,
+				email,
+				password
+			},
+			{ withCredentials: true }
+		);
+	}
 
-  get currentUser() {
-    return this.user$.asObservable();
-  }
+	// Function to Login User
+	// ! Need to Verify Route with Backend
+	login({ email, password }) {
+		return this.httpClient.post(
+			`${environment.API_URL}/auth/login`,
+			{
+				email,
+				password
+			},
+			{ withCredentials: true }
+		);
+	}
 
-  constructor(
-    private httpClient: HttpClient,
-    private cookieService: CookieService,
-    private router: Router
-  ) { }
+	// Used to check User Credentials
+	me() {
+		return this.httpClient.get(`${environment.API_URL}/auth/me`, { withCredentials: true });
+	}
 
+	// verificationToken, email
 
-  // Sets Current User in order to continue Session
-  setUser({ id, username}: User) {
-    this.user$.next({ id, username });
-  }
+	verify(token, email) {
+		return this.httpClient.post(`${environment.API_URL}/auth/verify`, {
+			email: email,
+			verificationToken: token
+		});
+	}
 
-  // Function to Register New User
-  // ! Need to Verify Route with Backend
-  register({ username, email, password }: CreateUser) {
-    return this.httpClient.post<{ user: User; token: string}>(`${environment.API_URL}/api/v1/auth/register`, { username, email, password })
-    .pipe(tap(({ token }) => this.cookieService.set('token', token)));
-  }
-
-  // Function to Login User
-  // ! Need to Verify Route with Backend
-  login({ email, password }: LoginUser) {
-    return this.httpClient.post<{ user: User; token: string }>(
-      `${environment.API_URL}/api/v1/auth/login`, {email, password}
-    )
-    .pipe(tap(({ token }) => this.cookieService.set('token', token)));
-  }
-
-  // Used to check User Credentials
-  me() {
-    return this.httpClient.get<{ user: User }>(
-      `${environment.API_URL}/api/v1/auth/me`
-    )
-  }
-
-  // verificationToken, email
-
-  verify(token, email) {
-    return this.httpClient.post(`${environment.API_URL}/api/v1/auth/verify`, {email: email, verificationToken: token})
-  }
-
-  // Removes User from Session, Deletes Cookie, Routes to Landing Page
-  logout() {
-    this.cookieService.delete('token');
-    this.user$.next(null);
-    this.router.navigate(['/'])
-  }
+	// Removes User from Session, Deletes Cookie, Routes to Landing Page
+	logout() {
+		this.cookieService.delete("token");
+		this.router.navigate(["/"]);
+	}
 }
